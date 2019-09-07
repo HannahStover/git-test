f3ebcb5606c1933662a0041708eb143ec0779034
Hannah Stover <hrosestover@gmail.com>
2019-09-07 10:49:14 -0700

    seventh commit

--- sortthreenumbers.js
+++
@@ -1,9 +0,0 @@
-let x = prompt('Enter first number');
-let y = prompt('Enter second number');
-let z = prompt('Enter third number');
-
-function largest() {
-	return alert(Math.max(x, y, z));
-}
-
-largest();
